a1eddb1a5b8604eae91d815eea3ff866c8105ec7
Hanjo Schumacher <dev@hanjo.dev>
2024-09-25 14:08:17 +0200

    version 0.8 of Würzburg Waste Collection cal

---
+++ MMM-WasteCollectionWuerzburg.js
@@ -0,0 +1,164 @@
+'use strict';
+
+Module.register("MMM-WasteCollectionWuerzburg", {
+    requiresVersion: "2.1.0",
+    defaults: {
+        title: "Abfallkalender Würzburg",
+        districts: [ // shows all if empty
+            // 'Altstadt',
+            'Frauenland',
+            // 'Dürrbach alle mit Hafen',
+            // 'Grombühl',
+            // 'Heidingsfeld',
+            // 'Heuchelhof innen',
+            // 'Heuchelhof aussen',
+            // 'Lengfeld',
+            // 'Lindleinsmühle',
+            // 'Mainviertel',
+            // 'Neumühle',
+            // 'Pilziggrund',
+            // 'Rottenbauer',
+            // 'Sanderau',
+            // 'Steinbachtal',
+            // 'Versbach',
+            // 'Zellerau'
+        ],
+        categories: [
+            'Restmüll',
+            'Biomüll',
+            'Gelbe Säcke',
+            'Papier',
+            // 'Problemmüll-Sammlung',
+            // 'Wertstoffmobil'
+        ],
+        updateInterval: 604800*1000, // once a week
+        rows: {},
+        showTypes: true,
+        showDistrict: false,
+        showYear: false,
+        rowsMax: 5 // max num rows to be displayed
+    },
+    getStyles: function() {
+        return [this.file('css/default.css')];
+    },
+    getDom: function() {
+        var tbl = document.createElement("table");
+        var rowCount = 0;
+
+        Object.keys(this.config.rows).forEach(k => {
+            var districts = this.config.rows[k];
+            var date = k;
+
+            Object.keys(districts).forEach(districtName => {
+                var categories = districts[districtName];
+
+                if (this.config.rowsMax && this.config.rowsMax > 0 && rowCount == this.config.rowsMax) {
+                    return;
+                }
+
+                var tr = this.createDomRow(date, categories);
+
+                if (tr) tbl.appendChild(tr);
+
+                rowCount++;
+            });
+        });
+
+        return tbl;
+    },
+    createDomRow: function(date, categories) {
+        var districtName = null;
+        var catArr = [];
+
+        var tr = document.createElement('tr');
+
+        /**
+         * waste type icons & names
+         */
+        var td = document.createElement("td");
+        td.className = 'waste-icons';
+
+        Object.keys(categories).forEach(categoryName => {
+            var category = categories[categoryName];
+
+            if (!districtName) {
+                districtName = category.stadtteil_name;
+            }
+
+            if (this.config.categories.length === 0 || this.config.categories.indexOf(category.kategorie) > -1) {
+                catArr.push(category.kategorie);
+            }
+
+            var img = document.createElement('img');
+
+            img.src = category.bild;
+
+            td.appendChild(img);
+        });
+
+        // skip row if no desired category available
+        if (catArr.length === 0) return false;
+
+        tr.appendChild(td);
+
+        /**
+         * waste type names
+         */
+        if (this.config.showTypes) {
+            var td = document.createElement("td");
+
+            td.innerHTML = catArr.join(', ');
+
+            tr.appendChild(td);
+        }
+
+        /**
+         * district name
+         */
+        if (this.config.showDistrict) {
+            var td = document.createElement("td");
+
+            td.innerHTML = districtName;
+            tr.appendChild(td);
+        }
+
+        /**
+         * collection date
+         */
+        var td = document.createElement("td");
+        var collectionDate = new Date(date);
+
+        var dateOptions = {month: "numeric", day: "numeric"};
+        if (this.config.showYear) {
+            dateOptions.year = 'numeric';
+        }
+
+        td.innerHTML = collectionDate.toLocaleDateString('de-DE', dateOptions);
+
+        tr.appendChild(td);
+
+
+        return tr;
+    },
+    getHeader: function () {
+        return this.config.title;
+    },
+    getData: function() {
+        this.sendSocketNotification('GET_DATA', {
+            districts: this.config.districts
+        });
+    },
+    socketNotificationReceived: function(name, payload) {
+        if (name !== 'API_DATA_RECEIVED') return this.rows = [];
+
+        this.config.rows = payload.rows;
+
+        this.updateDom();
+    },
+    start: function() {
+        const _this = this;
+        setInterval(function() {_this.getData()}, _this.config.updateInterval);
+
+        _this.getData();
+    }
+});
---
+++ node_helper.js
@@ -0,0 +1,88 @@
+'use strict';
+const Helper = require('node_helper');
+const Request = require('request');
+const Logger = require('logger');
+const Moment = require('moment');
+
+function deUmlaut(value){
+    value = value.toLowerCase();
+    value = value.replace(/ä/g, 'ae');
+    value = value.replace(/ö/g, 'oe');
+    value = value.replace(/ü/g, 'ue');
+    value = value.replace(/ß/g, 'ss');
+    value = value.replace(/ /g, '-');
+    value = value.replace(/\./g, '');
+    value = value.replace(/,/g, '');
+    value = value.replace(/\(/g, '');
+    value = value.replace(/\)/g, '');
+    return value;
+  }
+
+module.exports = Helper.create({
+    config: {
+        sourceUrl: 'https://opendata.wuerzburg.de/api/explore/',
+        apiVersion: 'v2.1',
+        limit: 50,
+        orderBy: 'start ASC',
+        fields: ['titel', 'kategorie', 'start', 'bild', 'stadtteil_name']
+    },
+    getData: function(data) {
+        var _this = this;
+
+        var urlSearchParams = new URLSearchParams({
+            limit: this.config.limit,
+            order_by: this.config.orderBy,
+            select: this.config.fields.join()
+        });
+
+        var districts = data.districts || [];
+        if (districts.length > 0) {
+            // const currentDate = new Date();
+            urlSearchParams.append('where', 'stadtteil_name IN (' + districts.map(a => `'${a}'`).join() + ')'); //  AND start <= ' + currentDate.setDate(currentDate.getDate() + 14))
+        }
+
+        var url = this.config.sourceUrl + this.config.apiVersion + '/catalog/datasets/abfallkalender-wuerzburg/records?' + urlSearchParams.toString();
+
+        Logger.log(`Fetching opendata.wuerzburg.de data via ${url} ...`);
+
+        Request({ url: url, method: 'GET' }, function (error, response, body) {
+            if (error || response.statusCode != 200) {
+                throw new Error('Invalid API request');
+                return;
+            }
+
+            var res = JSON.parse(body);
+
+            if (!res || !res.results || res.results.length === 0) return;
+
+            _this.sendSocketNotification('API_DATA_RECEIVED', {
+                rows: _this.processData(res.results)
+            });
+        });
+    },
+    processData: function(data) {
+        var byDate = {};
+
+        for (var i = 0; i < data.length; i++) {
+            data[i].cat = deUmlaut(data[i].kategorie);
+            data[i].district = deUmlaut(data[i].stadtteil_name);
+
+            if (typeof byDate[data[i].start]  !== 'object') {
+                byDate[data[i].start] = {};
+            }
+
+            if (typeof byDate[data[i].start][data[i].district] !== 'object') {
+                byDate[data[i].start][data[i].district] = {};
+            }
+
+            byDate[data[i].start][data[i].district][data[i].cat] = data[i];
+        }
+
+        return byDate;
+    },
+    socketNotificationReceived: function(name, payload) {
+        if (name === 'GET_DATA') {
+            this.getData(payload);
+        }
+    }
+});
